7b11e89656fd575d58b8052a1640f00ab8dcedfb
dparkour <damianjeremygerrits@gmail.com>
2019-03-28 16:14:45 +0000

    Add coin pick up system

---
+++ Assets/Scripts/ScoringSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringSystem : MonoBehaviour
+{
+    public GameObject scoreText;
+    public int theScore;
+    public AudioSource collectSound;
+
+    // Destroy object on collision and add a amount to the score
+    void OnTriggerEnter(Collider other)
+    {
+        collectSound.Play();
+        theScore += 1;
+        scoreText.GetComponent<Text>().text = "COIN: " + theScore;
+        Destroy(gameObject);
+    }
+}
