51ad81b1587b4b20d7e1be2edd2fea4c154ca7b6
dparkour <damianjeremygerrits@gmail.com>
2019-03-29 11:30:07 +0000

    level edit and crosshair

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
 
 public class Coin : MonoBehaviour
+
+
 {
     [SerializeField] private AudioClip clip = null;
 
+    //Add points when the player collides with the coin and play the audio
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag != "Player") return;
         FindObjectOfType<ScoreManager>().Score += 1;
         AudioSource.PlayClipAtPoint(clip, transform.position);
         Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0, 90 * Time.deltaTime, 0);
     }
 }
