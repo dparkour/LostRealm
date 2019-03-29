b67c9cc1a58804e230a1c8b4a17434a3e34d2f3a
dparkour <damianjeremygerrits@gmail.com>
2019-03-29 20:05:52 +0000

    level edit new path

---
+++ Assets/RestartTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartTrigger : MonoBehaviour
+{
+    private Scene scene;
+
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Application.LoadLevel(scene.name);
+        }
+        else;
+        {
+
+        }
+    }
+}
