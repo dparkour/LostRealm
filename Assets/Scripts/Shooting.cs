dc3f9c35759d5715a6e7959a5dcc5ec4eab30aaa
dparkour <damianjeremygerrits@gmail.com>
2019-03-29 16:23:10 +0000

    add cursor color switch when interacting with crystal

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -1,40 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shooting : MonoBehaviour
 {
-
     public LayerMask mask;
+    public Image cursor;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        cursor.color = new Color(255,255,255);
         RaycastHit hit;
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (Physics.Raycast(transform.position, transform.forward, out hit, 10, mask))
             {
-                if (hit.collider.gameObject.GetComponent<Crystal>())
+                if (Physics.Raycast(transform.position, transform.forward, out hit, 10, mask))
+               
                 {
-                    Crystal c = hit.collider.gameObject.GetComponent<Crystal>();
-
-                    Debug.Log("hoi");
-
-                    if (!c.activated)
-
+                    if (hit.collider.gameObject.GetComponent<Crystal>())
                     {
-                        c.activated = true;
+                        Crystal c = hit.collider.gameObject.GetComponent<Crystal>();
+
+                        cursor.color = new Color(255, 0, 0);
+
+                        if (!c.activated)
+                        if (Input.GetMouseButtonDown(0))
+
+                        {
+                            c.activated = true;
+                        }
                     }
                 }
             }
         }
     }
-}
