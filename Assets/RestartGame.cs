8a6bc0431a23226e439666cad79e5b6fc33af8c7
dparkour <damianjeremygerrits@gmail.com>
2019-06-15 19:00:43 +0200

    make credits Menu and restart game option

---
+++ Assets/RestartGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    public void GameRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
