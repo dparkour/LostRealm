f6b344914c66290216b4502e71c2c09fc49cc3cf
dparkour <damianjeremygerrits@gmail.com>
2019-06-16 13:42:11 +0200

    edits

---
+++ Assets/Scripts/CursorVisibility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorVisibility : MonoBehaviour
+{
+    // Use this for initialization
+    void Update()
+    {
+        //Set Cursor to not be visible
+        Cursor.visible = true;
+    }
+}
