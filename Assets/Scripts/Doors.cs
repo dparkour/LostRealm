224a21d54319e13973b4930c0204d01c6d939a1e
dparkour <damianjeremygerrits@gmail.com>
2019-06-17 14:04:19 +0200

    gate open animation

--- Assets/Scripts/Doors.cs
+++ Assets/Scripts/Doors.cs
@@ -6,10 +6,12 @@ public class Doors : MonoBehaviour
 {
     public Crystal crystal1;
     public Crystal crystal2;
+    private Animator anim;
+    [SerializeField] private GameObject uiTrigger;
 
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -17,7 +19,8 @@ public class Doors : MonoBehaviour
     {
         if(crystal1.activated && crystal2.activated)
         {
-            gameObject.SetActive(false);
+            anim.SetTrigger("OpenDoors");
+            uiTrigger.SetActive(false);
         }
     }
 }
