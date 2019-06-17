c51bd00c7e6de5c1bf36172cd2a38675102e6479
dparkour <damianjeremygerrits@gmail.com>
2019-06-17 13:45:58 +0200

    UI edit and light bake

---
+++ Assets/Scripts/PlayerShowUI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class UIElements
+{
+    [SerializeField]
+    public string TriggerTagName;
+    [SerializeField]
+    public GameObject UIElementGameObject;
+    [SerializeField]
+    public bool IsEnabled = false;
+}
+
+public class PlayerShowUI : MonoBehaviour
+{
+    public List<UIElements> TriggerUIElements;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach(UIElements uiElement in TriggerUIElements)
+        {
+            if(other.gameObject.tag == uiElement.TriggerTagName)
+            {
+                if (uiElement.UIElementGameObject.activeSelf)
+                    uiElement.UIElementGameObject.SetActive(false);
+                else
+                    uiElement.UIElementGameObject.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        foreach (UIElements uiElement in TriggerUIElements)
+        {
+            if (other.gameObject.tag == uiElement.TriggerTagName)
+            {
+                if (uiElement.UIElementGameObject.activeSelf)
+                    uiElement.UIElementGameObject.SetActive(false);
+                else
+                    uiElement.UIElementGameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UIDeactivation.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIDeactivation : MonoBehaviour
+{
+    public float FadeTime = 3f;
+    public float WaitDelay = 5f;
+
+    private Component currentComponent;
+    private Image currentImage;
+    private TextMeshProUGUI currentTextMeshUGUI;
+    private TextMeshPro currentTextMeshPro;
+    private Text currentText;
+
+    private void Start()
+    {
+        if (GetComponent<Image>())
+            currentImage = GetComponent<Image>();
+        else if (GetComponent<TextMeshProUGUI>())
+            currentTextMeshUGUI = GetComponent<TextMeshProUGUI>();
+        else if (GetComponent<Text>())
+            currentText = GetComponent<Text>();
+
+        StartCoroutine(FadeIn(WaitDelay));
+    }
+
+    private IEnumerator FadeIn(float time)
+    {
+        if (currentImage != null)
+            currentImage.CrossFadeAlpha(1, FadeTime, true);
+        else if (currentTextMeshPro != null)
+            currentTextMeshPro.CrossFadeAlpha(1, FadeTime, true);
+        else if (currentTextMeshUGUI != null)
+            currentTextMeshUGUI.CrossFadeAlpha(1, FadeTime, true);
+        yield return new WaitForSeconds(time);
+        StartCoroutine(FadeOut(WaitDelay));
+    }
+
+    private IEnumerator FadeOut(float time)
+    {
+        yield return new WaitForSeconds(time);
+        if (currentImage != null)
+            currentImage.CrossFadeAlpha(0, FadeTime, true);
+        else if (currentTextMeshPro != null)
+            currentTextMeshPro.CrossFadeAlpha(0, FadeTime, true);
+        else if (currentTextMeshUGUI != null)
+            currentTextMeshUGUI.CrossFadeAlpha(0, FadeTime, true);
+    }
+}
